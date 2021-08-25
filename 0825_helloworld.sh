42fdfc561de0f86e4c87f27946c5661c18e63e70
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-08-26 08:46:01 +0900

    sleep文

--- 0825_helloworld.sh
+++ 0825_helloworld.sh
@@ -13,6 +13,8 @@
 # echo
 echo "次にif文を学習します。まずhogeと打ち込んでみてください"
 read str
+echo "sleepを入れているので3秒お待ちください"; sleep 3
+echo
 if [ "$str" = "hoge" ]; then
   echo "hoge,素晴らしい"
 elif [ "$str" = "huga" ]; then
@@ -20,3 +22,15 @@ elif [ "$str" = "huga" ]; then
 else
   echo "話を聞いていましたか？"
 fi
+echo
+echo "今度はhugaと打ち込んでみてください"
+read str
+echo
+if [ "$str" = "hoge" ]; then
+  echo "hoge,素晴らしい"
+elif [ "$str" = "huga" ]; then
+  echo "huga,エクセレント"
+else
+  echo "話を聞いていましたか？"
+fi
+
