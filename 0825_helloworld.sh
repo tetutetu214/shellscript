b80fac4fe08db78dbac804637901d33f02e1f85a
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-08-26 08:39:30 +0900

    if文の記述

--- 0825_helloworld.sh
+++ 0825_helloworld.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-echo "名前を入力してください"
-read NAME
-echo
-echo "Hello $NAME World!"
+# echo "名前を入力してください"
+# read NAME
+# echo
+# echo "Hello $NAME World!"
+# echo 
+# echo "それでは学習をスタートします。ls -lと打刻してください"
+# read opt
+# $opt
+# echo
+# echo "ディレクトリ（ファイル）に格納されているファイルが表示されましたか？"
+# echo
+echo "次にif文を学習します。まずhogeと打ち込んでみてください"
+read str
+if [ "$str" = "hoge" ]; then
+  echo "hoge,素晴らしい"
+elif [ "$str" = "huga" ]; then
+  echo "huga,エクセレント"
+else
+  echo "話を聞いていましたか？"
+fi
