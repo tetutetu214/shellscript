73028c8039eee57767d1847609231e7e0f8376e0
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-08-27 08:51:21 +0900

    ディレクトリif文で確認

---
+++ 0827_rename.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Hello,World!"
+echo "ディレクトリを作成します"; sleep 2
+if  [ -d ./lesson_bash ]; then
+  echo "既に存在しています"
+else
+  mkdir lesson_bash
+fi
+echo "ls -l と打ち込んでください。lesson_bashというディレクトリが表示されていることを確認してください"
+read opt
+$opt
