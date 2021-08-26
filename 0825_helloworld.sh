84465d2855b937fe6e881a0470ec7ad4b0a9e1ba
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-08-26 20:57:13 +0900

    case文

--- 0825_helloworld.sh
+++ 0825_helloworld.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# readを利用したキーボード入力
 # echo "名前を入力してください"
 # read NAME
 # echo
@@ -11,26 +12,37 @@
 # echo
 # echo "ディレクトリ（ファイル）に格納されているファイルが表示されましたか？"
 # echo
-echo "次にif文を学習します。まずhogeと打ち込んでみてください"
-read str
-echo "sleepを入れているので3秒お待ちください"; sleep 3
-echo
-if [ "$str" = "hoge" ]; then
-  echo "hoge,素晴らしい"
-elif [ "$str" = "huga" ]; then
-  echo "huga,エクセレント"
-else
-  echo "話を聞いていましたか？"
-fi
-echo
-echo "今度はhugaと打ち込んでみてください"
-read str
-echo
-if [ "$str" = "hoge" ]; then
-  echo "hoge,素晴らしい"
-elif [ "$str" = "huga" ]; then
-  echo "huga,エクセレント"
-else
-  echo "話を聞いていましたか？"
-fi
 
+# if文の利用
+# echo "次にif文を学習します。まずhogeと打ち込んでみてください"
+# read str
+# echo "sleepを入れているので3秒お待ちください"; sleep 3
+# echo
+# if [ "$str" = "hoge" ]; then
+#   echo "hoge,素晴らしい"
+# elif [ "$str" = "huga" ]; then
+#   echo "huga,エクセレント"
+# else
+#   echo "話を聞いていましたか？"
+# fi
+# echo
+# echo "今度はfugaと打ち込んでみてください"
+# read str
+# echo
+# if [ "$str" = "hoge" ]; then
+#   echo "hoge,素晴らしい"
+# elif [ "$str" = "fuga" ]; then
+#   echo "fuga,エクセレント"
+# else
+#   echo "話を聞いていましたか？"
+# fi
+
+# case文の利用
+echo "次はcase文を利用します、結果は同じに見えますが処理が異なります。さっそくhogeと打ち込んでください"
+read str
+echo "今回も3秒お待ちください";sleep 3
+case "$str" in
+  "hoge" ) echo "ないすhoge";;
+  "fuga" ) echo "えくせれんとfuga";;
+  * ) echo "話を聞いていましたか？";;
+esac
