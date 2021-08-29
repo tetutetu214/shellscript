9ed5f5329acdbf453b52e5bc85c8e93b38ea6fd7
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2021-08-29 14:20:09 +0900

    linux練習

--- if-gt.sh
+++ if-gt.sh
@@ -3,9 +3,9 @@
 echo "0~9の数字を入力してください"
 read NUM
 
-if [ $NUM -qe 5 ]; then
+if [ $NUM -ge 5 ]; then
   echo "入力した値は5以上です"
-elif; then
+elif [ $NUM -lt 4 ]; then
   echo "4未満です"
 else
   echo "指示にしたがっていません" 
